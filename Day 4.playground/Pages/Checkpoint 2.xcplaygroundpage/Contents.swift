7c1bc0f652590303147a62666876ef69abbc7ac2
“jdocampom” <gummy_pond0v@icloud.com>
2021-11-18 16:34:43 -0500

    Day 4 complete: Type annotations and Checkpoint 2.

---
+++ Day 4.playground/Pages/Checkpoint 2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,9 @@
+//: [Previous](@previous)
+
+import Foundation
+
+let albums = ["Red", "Fearless", "Greatest Hits", "Fearless"]
+print("Number of items in \"albums\": \(albums.count)")
+print("Number of unique items in \"albums\": \(Set(albums).count)")
+
+//: [Next](@next)

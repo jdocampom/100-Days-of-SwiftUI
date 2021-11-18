ef914f86c77895ba5112fa3021921101bae56e02
“jdocampom” <gummy_pond0v@icloud.com>
2021-11-18 16:48:15 -0500

    Day 5 completed: If-Else, Switch and the Ternary Operator.

---
+++ Day 5.playground/Pages/If Statement.xcplaygroundpage/Contents.swift
@@ -0,0 +1,90 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//if someCondition {
+//    print("Do something")
+//}
+
+let someCondition = true
+
+if someCondition {
+    print("Do something")
+    print("Do something else")
+    print("Do a third thing")
+}
+
+let score = 85
+
+if score > 80 {
+    print("Great job!")
+}
+
+let speed = 88
+let percentage = 85
+let age = 18
+
+if speed >= 88 {
+    print("Where we're going we don't need roads.")
+}
+
+if percentage < 85 {
+    print("Sorry, you failed the test.")
+}
+
+if age >= 18 {
+    print("You're eligible to vote")
+}
+
+let ourName = "Dave Lister"
+let friendName = "Arnold Rimmer"
+
+if ourName < friendName {
+    print("It's \(ourName) vs \(friendName)")
+}
+
+if ourName > friendName {
+    print("It's \(friendName) vs \(ourName)")
+}
+
+var numbers = [1, 2, 3]
+numbers.append(4)
+
+if numbers.count > 3 {
+    numbers.remove(at: 0)
+}
+print(numbers)
+
+let country = "Canada"
+
+if country == "Australia" {
+    print("G'day!")
+}
+
+let name = "Taylor Swift"
+
+if name != "Anonymous" {
+    print("Welcome, \(name)")
+}
+
+var username = "taylorswift13"
+
+if username == "" {
+    username = "Anonymous"
+}
+
+print("Welcome, \(username)!")
+
+if username.count == 0 {
+    username = "Anonymous"
+}
+
+if username.isEmpty == true {
+    username = "Anonymous"
+}
+
+if username.isEmpty {
+    username = "Anonymous"
+}
+
+//: [Next](@next)

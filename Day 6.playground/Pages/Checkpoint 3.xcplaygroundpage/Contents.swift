1f44a0c30cf754d0744abbaab70b1993b5947bc1
“jdocampom” <gummy_pond0v@icloud.com>
2021-11-19 18:45:02 -0500

    Day 6 completed: Loops and Checkpoint 3

---
+++ Day 6.playground/Pages/Checkpoint 3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,18 @@
+//: [Previous](@previous)
+
+import Foundation
+
+for i in 1...100 {
+    switch (i % 3 == 0, i % 5 == 0) {
+    case (true, false):
+    print("Fizz")
+    case (false, true):
+    print("Buzz")
+    case (true, true):
+    print("FizzBuzz")
+    default:
+    print(i)
+    }
+}
+
+//: [Next](@next)

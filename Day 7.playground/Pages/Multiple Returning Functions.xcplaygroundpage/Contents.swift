e1caac5e1e539c88a127a9c2c16a38946893a93d
“jdocampom” <gummy_pond0v@icloud.com>
2021-11-19 18:54:34 -0500

    Day 7 completed:  functions, parameters, and return values

---
+++ Day 7.playground/Pages/Multiple Returning Functions.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+//: [Previous](@previous)
+
+import Foundation
+
+func isUppercase(string: String) -> Bool {
+    string == string.uppercased()
+}
+
+func getUser() -> [String] {
+    ["Taylor", "Swift"]
+}
+
+let user = getUser()
+print("Name: \(user[0]) \(user[1])")
+
+func getUser2() -> [String: String] {
+    [
+        "firstName": "Taylor",
+        "lastName": "Swift"
+    ]
+}
+
+let user2 = getUser2()
+print("Name: \(user2["firstName", default: "Anonymous"]) \(user2["lastName", default: "Anonymous"])")
+
+func getUser3() -> (firstName: String, lastName: String) {
+    (firstName: "Taylor", lastName: "Swift")
+}
+
+let user3 = getUser3()
+print("Name: \(user3.firstName) \(user3.lastName)")
+
+func getUser4() -> (String, String) {
+    ("Taylor", "Swift")
+}
+
+let user4 = getUser4()
+print("Name: \(user4.0) \(user4.1)")
+
+//: [Next](@next)

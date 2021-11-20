366fe2e961150545d4079899f562e0bfd364fbd2
“jdocampom” <gummy_pond0v@icloud.com>
2021-11-20 09:01:32 -0500

    Day 10 completed: Structs, Computed Properties and Property Observers

---
+++ Day 10.playground/Pages/Property Observers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+//: [Previous](@previous)
+
+import Foundation
+
+struct Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var game = Game()
+game.score += 10
+game.score -= 3
+game.score += 1
+
+print()
+
+struct App {
+    var contacts = [String]() {
+        willSet {
+            print("Current value is: \(contacts)")
+            print("New value will be: \(newValue)")
+        }
+
+        didSet {
+            print("There are now \(contacts.count) contacts.")
+            print("Old value was \(oldValue)")
+        }
+    }
+}
+
+var app = App()
+app.contacts.append("Adrian E")
+app.contacts.append("Allen W")
+app.contacts.append("Ish S")
+
+//: [Next](@next)

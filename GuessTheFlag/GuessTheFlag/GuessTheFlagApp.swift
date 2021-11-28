89832ef3aadfc1e464dc12c0787a7ba599dd3967
jdocampom <gummy_pond0v@icloud.com>
2021-11-27 21:52:21 -0500

    Days 20-22 como¡pleted: Guess the Flag App

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Juan Diego Ocampo on 10/24/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

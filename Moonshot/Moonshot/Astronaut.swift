8fbcd29b8a69a0a41b92b24bb9513954c0ac1e8a
jdocampom <gummy_pond0v@icloud.com>
2021-12-11 12:30:09 -0500

    Day 38-41 completed: Moonshot App - no challenges done yet

---
+++ Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Juan Diego Ocampo on 12/12/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Juan Diego Ocampo on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

ae7a3713a5d5056e5676e801e85c4c6b6cbaf8e3
jdocampom <gummy_pond0v@icloud.com>
2022-01-04 08:18:25 -0500

    Days 74-76 completed: Adding Accesibility assets to GuessTheFlag, Bookworm and WordScramble

---
+++ Accesibility Test/Accesibility Test/Accesibility_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accesibility_TestApp.swift
+//  Accesibility Test
+//
+//  Created by Juan Diego Ocampo on 4/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Accesibility_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

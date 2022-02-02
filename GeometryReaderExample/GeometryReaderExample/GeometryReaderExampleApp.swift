65b868fcd3dc76795eb32bd5dcf1ffac7f4d3c27
“jdocampom” <gummy_pond0v@icloud.com>
2022-02-02 07:15:47 -0500

    Project 18 comppleted: GeometryReader

---
+++ GeometryReaderExample/GeometryReaderExample/GeometryReaderExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderExampleApp.swift
+//  GeometryReaderExample
+//
+//  Created by Juan Diego Ocampo on 2/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

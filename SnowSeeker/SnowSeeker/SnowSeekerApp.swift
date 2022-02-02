20ea97b37bc730c5dad43c4a3948094885d5492b
“jdocampom” <gummy_pond0v@icloud.com>
2022-02-02 07:23:13 -0500

    Project 19 comppleted: SnowSeeker App

---
+++ SnowSeeker/SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Juan Diego Ocampo on 03/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
